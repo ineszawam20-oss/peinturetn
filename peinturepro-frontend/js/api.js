5b1c12cc503da076a3c4d1aff5b2545bc96f842f
ineszawam20-oss <ineszawam20@gmail.com>
2026-04-13 19:42:14 +0200

    Description claire de ce qui a changé

--- peinturepro-frontend/js/api.js
+++ peinturepro-frontend/js/api.js
@@ -3,7 +3,7 @@
 // Gestion automatique JWT + Refresh Token
 // ============================================
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'http://localhost:3001/api';
 
 // ===== Helpers Token =====
 const Auth = {
