950b3d72029ecff7ab04b3b8ddadfc5692c45569
ineszawam20-oss <ineszawam20@gmail.com>
2026-04-23 13:34:39 +0200

    fix: ajout image et description dans les requêtes produits

--- peinturepro-backend/models/queries/produits.queries.js
+++ peinturepro-backend/models/queries/produits.queries.js
@@ -11,21 +11,24 @@ const ProduitsQueries = {
   create: (data) =>
     pool.query(
       `INSERT INTO produits
-       (nom, type, finition, couleur_code, rendement_m2_L, prix_litre, stock_litres, stock_minimum)
-       VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+       (nom, type, finition, couleur_code, rendement_m2_L, prix_litre, stock_litres, stock_minimum, image, description)
+       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
       [data.nom, data.type, data.finition, data.couleur_code,
        data.rendement_m2_L || 10, data.prix_litre,
-       data.stock_litres || 0, data.stock_minimum || 50]
+       data.stock_litres || 0, data.stock_minimum || 50,
+       data.image || null, data.description || null]
     ),
 
   update: (id, data) =>
     pool.query(
       `UPDATE produits SET nom=?, type=?, finition=?, couleur_code=?,
-       rendement_m2_L=?, prix_litre=?, stock_litres=?, stock_minimum=?
+       rendement_m2_L=?, prix_litre=?, stock_litres=?, stock_minimum=?,
+       image=?, description=?
        WHERE id=?`,
       [data.nom, data.type, data.finition, data.couleur_code,
        data.rendement_m2_L, data.prix_litre,
-       data.stock_litres, data.stock_minimum, id]
+       data.stock_litres, data.stock_minimum,
+       data.image || null, data.description || null, id]
     ),
 
   remove: (id) =>
